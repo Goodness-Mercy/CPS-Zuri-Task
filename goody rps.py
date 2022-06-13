4e1b7816b4cec04cc8d9457031c06b2f1a3d7027
Goodnessmercy <goodiemercy758@gmail.com>
2022-06-13 18:36:43 +0100

    Create goody rps.py
    Newly created task

---
+++ goody rps.py
@@ -0,0 +1,33 @@
+import random
+choices = ["Rock", "Paper", "Scissors"]
+computer = random.choice(choices)
+player = False
+while True:
+    player = input("Rock, Paper or  Scissors?").capitalize()
+   
+    if player == computer:
+        print("Tie!")
+    elif player == "Rock":
+        if computer == "Paper":
+            print("You lose!", computer, "covers", player)
+            
+        else:
+            print("You win!", player, "smashes", computer)
+            
+    elif player == "Paper":
+        if computer == "Scissors":
+            print("You lose!", computer, "cut", player)
+            
+        else:
+            print("You win!", player, "covers", computer)
+            
+    elif player == "Scissors":
+        if computer == "Rock":
+            print("You lose...", computer, "smashes", player)
+            
+        else:
+            print("You win!", player, "cut", computer)
+            
+    elif player=='End':
+       
+        break
